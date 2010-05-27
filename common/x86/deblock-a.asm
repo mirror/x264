6589ad6dc6a2ac7599c5a19566306c274bd86853
Fiona Glaser <fiona@x264.com>
2010-05-27 14:27:32 -0700

    x86 assembly code for NAL escaping Up to ~10x faster than C depending on CPU. Helps the most at very high bitrates (e.g. lossless). Also make the C code faster and simpler.

--- common/x86/deblock-a.asm
+++ common/x86/deblock-a.asm
@@ -4,6 +4,7 @@
 ;* Copyright (C) 2005-2008 x264 project
 ;*
 ;* Authors: Loren Merritt <lorenm@u.washington.edu>
+;*          Fiona Glaser <fiona@x264.com>
 ;*
 ;* This program is free software; you can redistribute it and/or modify
 ;* it under the terms of the GNU General Public License as published by
