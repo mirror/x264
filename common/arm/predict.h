astrataro/x264_tMod

arm: implement x264_predict_4x4_v_armv6
Alone probably not worth it but allows use of predict_4x4_dc|h_armv6
in intra_sad|satd_x3_4x4_neon.

common/arm/predict.h
@@ -26,6 +26,9 @@
 #ifndef X264_ARM_PREDICT_H
 #define X264_ARM_PREDICT_H
 
+void x264_predict_4x4_h_armv6( pixel *src );
+void x264_predict_4x4_v_armv6( pixel *src );
+void x264_predict_4x4_dc_armv6( pixel *src );
 void x264_predict_8x8_v_neon( pixel *src, pixel edge[36] );
 void x264_predict_8x8_h_neon( pixel *src, pixel edge[36] );
 void x264_predict_8x8_dc_neon( pixel *src, pixel edge[36] );

mirror/x264

common/arm/predict.h
@@ -26,6 +26,9 @@
 #ifndef X264_ARM_PREDICT_H
 #define X264_ARM_PREDICT_H
 
+void x264_predict_4x4_dc_armv6( uint8_t *src );
+void x264_predict_4x4_v_armv6( uint8_t *src );
+void x264_predict_4x4_h_armv6( uint8_t *src );
 void x264_predict_8x8_v_neon( pixel *src, pixel edge[36] );
 void x264_predict_8x8_h_neon( pixel *src, pixel edge[36] );
 void x264_predict_8x8_dc_neon( pixel *src, pixel edge[36] );