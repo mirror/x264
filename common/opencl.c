c15f20bd772487d863f01a2813a3ab45b1f11a6b
Diego Biurrun <diego@biurrun.de>
2014-04-27 23:23:49 +0200

    opencl: Check return value of fread()
    common/opencl.c:138:10: warning: ignoring return value of 'fread', declared with attribute warn_unused_result [-Wunused-result]

--- common/opencl.c
+++ common/opencl.c
@@ -135,7 +135,8 @@ static cl_program x264_opencl_cache_load( x264_t *h, const char *dev_name, const
     rewind( fp );
     CHECKED_MALLOC( binary, size );
 
-    fread( binary, 1, size, fp );
+    if ( fread( binary, 1, size, fp ) != size )
+        goto fail;
     const uint8_t *ptr = (const uint8_t*)binary;
 
 #define CHECK_STRING( STR )\
