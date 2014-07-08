6eb483e4ca23f34a6a8fe09f3f2e9c9f192fd76b
Henrik Gramner <henrik@gramner.com>
2014-07-08 21:15:32 +0200

    x86: Fix SIGILL in high bit-depth intra_sad_x3_4x4_sse2
    An SSE3 instruction was used in an SSE2 function.

--- common/x86/sad16-a.asm
+++ common/x86/sad16-a.asm
@@ -546,7 +546,12 @@ SAD_X 4, 16,  8
 
 %macro INTRA_SAD_X3_4x4 0
 cglobal intra_sad_x3_4x4, 3,3,7
+%if cpuflag(ssse3)
     movddup   m0, [r1-1*FDEC_STRIDEB]
+%else
+    movq      m0, [r1-1*FDEC_STRIDEB]
+    punpcklqdq m0, m0
+%endif
     movq      m1, [r0+0*FENC_STRIDEB]
     movq      m2, [r0+2*FENC_STRIDEB]
     pshuflw   m6, m0, q1032
