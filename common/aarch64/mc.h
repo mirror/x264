ce6c94c0bef3350e9546302aae5909404b056fdb
Janne Grunau <janne-x264@jannau.net>
2014-09-02 10:27:22 +0200

    aarch64: add myself as author to aarch64/mc.h

--- common/aarch64/mc.h
+++ common/aarch64/mc.h
@@ -3,6 +3,8 @@
  *****************************************************************************
  * Copyright (C) 2014 x264 project
  *
+ * Authors: Janne Grunau <janne-x264@jannau.net>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
