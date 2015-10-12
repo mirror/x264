366fa85885053c7b836a4272a4fbec1852103979
Geza Lore <gezalore@gmail.com>
2015-10-12 13:13:42 +0100

    x86inc: Add debug symbols indicating sizes of compiled functions
    Some debuggers/profilers use this metadata to determine which function a
    given instruction is in; without it they get can confused by local labels
    (if you haven't stripped those). On the other hand, some tools are still
    confused even with this metadata. e.g. this fixes `gdb`, but not `perf`.
    
    Currently only implemented for ELF.

--- tools/checkasm-a.asm
+++ tools/checkasm-a.asm
@@ -68,14 +68,14 @@ cextern_naked puts
 ;-----------------------------------------------------------------------------
 cglobal checkasm_stack_clobber, 1,2
     ; Clobber the stack with junk below the stack pointer
-    %define size (max_args+6)*8
-    SUB  rsp, size
-    mov   r1, size-8
+    %define argsize (max_args+6)*8
+    SUB  rsp, argsize
+    mov   r1, argsize-8
 .loop:
     mov [rsp+r1], r0
     sub   r1, 8
     jge .loop
-    ADD  rsp, size
+    ADD  rsp, argsize
     RET
 
 %if WIN64
