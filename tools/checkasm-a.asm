a472b60daae0cac17d91ddf62ad4f474ded63e5b
Henrik Gramner <henrik@gramner.com>
2017-03-24 22:27:42 +0100

    checkasm: Fix compilation on hardened x86-64 ELF systems
    Normal PC-relative relocations cannot be used for resolving the address of
    external symbols on systems where ASLR results in the offset being larger
    than 32 bits. We are required to to go through the PLT instead.

--- tools/checkasm-a.asm
+++ tools/checkasm-a.asm
@@ -153,7 +153,11 @@ cglobal checkasm_call, 2,15,16,max_args*8+8
     mov  r9, rax
     mov r10, rdx
     lea  r0, [error_message]
+%if FORMAT_ELF
+    call puts wrt ..plt
+%else
     call puts
+%endif
     mov  r1, [rsp+max_args*8]
     mov  dword [r1], 0
     mov  rdx, r10
