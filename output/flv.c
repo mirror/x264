5d4031618e9feedcb527fd4e5a91bc06e30b70b4
Anton Mitrofanov <BugMaster@narod.ru>
2017-08-17 23:51:14 +0300

    flv: Fix one frame video total duration

--- output/flv.c
+++ output/flv.c
@@ -322,7 +322,12 @@ static int close_file( hnd_t handle, int64_t largest_pts, int64_t second_largest
 
     CHECK( flv_flush_data( c ) );
 
-    double total_duration = (2 * largest_pts - second_largest_pts) * p_flv->d_timebase;
+    double total_duration;
+    /* duration algorithm fails with one frame */
+    if( p_flv->i_framenum == 1 )
+        total_duration = p_flv->i_fps_num ? (double)p_flv->i_fps_den / p_flv->i_fps_num : 0;
+    else
+        total_duration = (2 * largest_pts - second_largest_pts) * p_flv->d_timebase;
 
     if( x264_is_regular_file( c->fp ) && total_duration > 0 )
     {
