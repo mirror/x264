d1a53926fb90e9f4a4f1605f4b2a8a945a73e1d2
Henrik Gramner <henrik@gramner.com>
2018-06-25 00:09:51 +0200

    Prefer a monotonic clock source if available

--- common/osdep.c
+++ common/osdep.c
@@ -51,6 +51,10 @@ int64_t x264_mdate( void )
     struct timeb tb;
     ftime( &tb );
     return ((int64_t)tb.time * 1000 + (int64_t)tb.millitm) * 1000;
+#elif HAVE_CLOCK_GETTIME
+    struct timespec ts;
+    clock_gettime( CLOCK_MONOTONIC, &ts );
+    return (int64_t)ts.tv_sec * 1000000 + (int64_t)ts.tv_nsec / 1000;
 #else
     struct timeval tv_date;
     gettimeofday( &tv_date, NULL );
