d1e045e8e3978065f3ba5388a1b0e65b68a15e60
Hubert Mazur <hum@semihalf.com>
2022-11-16 12:33:32 +0000

    quant: Add neon implementations of coeff_last
    Provide arm64 neon implementations for coeff_last functions for high bit
    depth. Benchmarks are shown below.
    
    coeff_last4_c: 79
    coeff_last4_neon: 107
    coeff_last8_c: 109
    coeff_last8_neon: 154
    coeff_last15_c: 161
    coeff_last15_neon: 135
    coeff_last16_c: 160
    coeff_last16_neon: 132
    coeff_last64_c: 782
    coeff_last64_neon: 400
    
    Signed-off-by: Hubert Mazur <hum@semihalf.com>

--- common/aarch64/quant.h
+++ common/aarch64/quant.h
@@ -55,16 +55,24 @@ int x264_decimate_score16_neon( dctcoef * );
 #define x264_decimate_score64_neon x264_template(decimate_score64_neon)
 int x264_decimate_score64_neon( dctcoef * );
 
+// BIT DEPTH = 8
 #define x264_coeff_last4_aarch64 x264_template(coeff_last4_aarch64)
-int x264_coeff_last4_aarch64( int16_t * );
+int x264_coeff_last4_aarch64( dctcoef * );
 #define x264_coeff_last8_aarch64 x264_template(coeff_last8_aarch64)
-int x264_coeff_last8_aarch64( int16_t * );
+int x264_coeff_last8_aarch64( dctcoef * );
+
+// BIT DEPTH = 10
+#define x264_coeff_last4_neon x264_template(coeff_last4_neon)
+int x264_coeff_last4_neon( dctcoef * );
+#define x264_coeff_last8_neon x264_template(coeff_last8_neon)
+int x264_coeff_last8_neon( dctcoef * );
+
 #define x264_coeff_last15_neon x264_template(coeff_last15_neon)
-int x264_coeff_last15_neon( int16_t * );
+int x264_coeff_last15_neon( dctcoef * );
 #define x264_coeff_last16_neon x264_template(coeff_last16_neon)
-int x264_coeff_last16_neon( int16_t * );
+int x264_coeff_last16_neon( dctcoef * );
 #define x264_coeff_last64_neon x264_template(coeff_last64_neon)
-int x264_coeff_last64_neon( int16_t * );
+int x264_coeff_last64_neon( dctcoef * );
 
 #define x264_coeff_level_run4_aarch64 x264_template(coeff_level_run4_aarch64)
 int x264_coeff_level_run4_aarch64( int16_t *, x264_run_level_t * );
