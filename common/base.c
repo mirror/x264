ec49e0eb8dca680e10ad3916ea34aaaca45a539b
bUd <daoko@protonmail.com>
2023-04-12 09:23:57 +0200

    Add AQ bias strength parameter

--- common/base.c
+++ common/base.c
@@ -414,6 +414,7 @@ REALIGN_STACK void x264_param_default( x264_param_t *param )
     param->rc.f_pb_factor = 1.3;
     param->rc.i_aq_mode = X264_AQ_VARIANCE;
     param->rc.f_aq_strength = 1.0;
+    param->rc.f_aq_bias_strength = 1.0;
     param->rc.i_lookahead = 40;
 
     param->rc.b_stat_write = 0;
@@ -1346,6 +1347,8 @@ REALIGN_STACK int x264_param_parse( x264_param_t *p, const char *name, const cha
         p->rc.i_aq_mode = atoi(value);
     OPT("aq-strength")
         p->rc.f_aq_strength = atof(value);
+    OPT("aq-strength")
+        p->rc.f_aq_bias_strength = atof(value);
     OPT("pass")
     {
         int pass = x264_clip3( atoi(value), 0, 3 );
@@ -1556,7 +1559,11 @@ char *x264_param2string( x264_param_t *p, int b_res )
             s += sprintf( s, " pb_ratio=%.2f", p->rc.f_pb_factor );
         s += sprintf( s, " aq=%d", p->rc.i_aq_mode );
         if( p->rc.i_aq_mode )
-            s += sprintf( s, ":%.2f", p->rc.f_aq_strength );
+            {
+                s += sprintf( s, ":%.2f", p->rc.f_aq_strength );
+                if( p->rc.i_aq_mode == X264_AQ_AUTOVARIANCE_BIASED )
+                    s += sprintf( s, ":%.2f", p->rc.f_aq_bias_strength );
+            }
         if( p->rc.psz_zones )
             s += sprintf( s, " zones=%s", p->rc.psz_zones );
         else if( p->rc.i_zones )
