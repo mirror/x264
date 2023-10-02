fa7f1fce7f492b75947d041887db9e5c51515146
Loongson Technology Corporation Limited <yinshiyou-hf@loongson.cn>
2023-10-02 23:14:56 +0800

    loongarch: Improve the performance of dct series functions
    Performance has improved from 10.53fps to 11.27fps.
    Tested with following command:
    ./configure && make -j5
    ./x264 --threads 4 -o out.mkv yuv_1920x1080.yuv
    
    functions           performance     performance
                            (c)            (asm)
    add4x4_idct              34              9
    add8x8_idct              139             31
    add8x8_idct8             269             39
    add8x8_idct_dc           67              7
    add16x16_idct            564             123
    add16x16_idct_dc         260             22
    dct4x4dc                 18              10
    idct4x4dc                16              9
    sub4x4_dct               25              7
    sub8x8_dct               101             12
    sub8x8_dct8              160             25
    sub16x16_dct             403             52
    sub16x16_dct8            646             68
    zigzag_scan_4x4_frame    4               1
    
    Signed-off-by: zhoupeng <zhoupeng@loongson.cn>

--- Makefile
+++ Makefile
@@ -205,6 +205,7 @@ SRCASM_X += common/loongarch/deblock-a.S \
             common/loongarch/predict-a.S \
             common/loongarch/quant-a.S \
             common/loongarch/mc-a.S \
+            common/loongarch/dct-a.S \
 
 SRCS_X += common/loongarch/predict-c.c \
           common/loongarch/mc-c.c \
---
+++ common/loongarch/dct.h
@@ -0,0 +1,95 @@
+/*****************************************************************************
+ * dct.h: loongarch transform and zigzag
+ *****************************************************************************
+ * Copyright (C) 2023 x264 project
+ *
+ * Authors: Peng Zhou <zhoupeng@loongson.cn>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ * This program is also available under a commercial proprietary license.
+ * For more information, contact us at licensing@x264.com.
+ *****************************************************************************/
+
+#ifndef X264_LOONGARCH_DCT_H
+#define X264_LOONGARCH_DCT_H
+
+#define x264_sub8x8_dct_lasx x264_template(sub8x8_dct_lasx)
+void x264_sub8x8_dct_lasx( int16_t p_dst[4][16], uint8_t *p_src, uint8_t *p_ref );
+#define x264_sub16x16_dct_lasx x264_template(sub16x16_dct_lasx)
+void x264_sub16x16_dct_lasx( int16_t p_dst[16][16], uint8_t *p_src, uint8_t *p_ref );
+
+#define x264_sub8x8_dct8_lsx x264_template(sub8x8_dct8_lsx)
+void x264_sub8x8_dct8_lsx( int16_t pi_dct[64], uint8_t *p_pix1, uint8_t *p_pix2 );
+#define x264_sub16x16_dct8_lasx x264_template(sub16x16_dct8_lasx)
+void x264_sub16x16_dct8_lasx( int16_t pi_dct[4][64], uint8_t *p_pix1,
+                              uint8_t *p_pix2 );
+
+#define x264_add4x4_idct_lsx x264_template(add4x4_idct_lsx)
+void x264_add4x4_idct_lsx( uint8_t *p_dst, int16_t pi_dct[16] );
+#define x264_add8x8_idct_lasx x264_template(add8x8_idct_lasx)
+void x264_add8x8_idct_lasx( uint8_t *p_dst, int16_t pi_dct[4][16] );
+#define x264_add16x16_idct_lasx x264_template(add16x16_idct_lasx)
+void x264_add16x16_idct_lasx( uint8_t *p_dst, int16_t pi_dct[16][16] );
+#define x264_add8x8_idct8_lasx x264_template(add8x8_idct8_lasx)
+void x264_add8x8_idct8_lasx( uint8_t *p_dst, int16_t pi_dct[64] );
+#define x264_add8x8_idct_dc_lasx x264_template(add8x8_idct_dc_lasx)
+void x264_add8x8_idct_dc_lasx( uint8_t *p_dst, int16_t dct[4] );
+#define x264_add16x16_idct_dc_lasx x264_template(add16x16_idct_dc_lasx)
+void x264_add16x16_idct_dc_lasx( uint8_t *p_dst, int16_t dct[16] );
+
+#define x264_idct4x4dc_lasx x264_template(idct4x4dc_lasx)
+void x264_idct4x4dc_lasx( int16_t d[16] );
+#define x264_dct4x4dc_lasx x264_template(dct4x4dc_lasx)
+void x264_dct4x4dc_lasx( int16_t d[16] );
+
+#define x264_zigzag_scan_4x4_frame_lasx x264_template(zigzag_scan_4x4_frame_lasx)
+void x264_zigzag_scan_4x4_frame_lasx( int16_t level[16], int16_t dct[16] );
+
+#define x264_sub4x4_dct_lsx x264_template(sub4x4_dct_lsx)
+void x264_sub4x4_dct_lsx( int16_t p_dst[16], uint8_t *p_src, uint8_t *p_ref );
+#define x264_sub8x8_dct_lsx x264_template(sub8x8_dct_lsx)
+void x264_sub8x8_dct_lsx( int16_t p_dst[4][16], uint8_t *p_src, uint8_t *p_ref );
+#define x264_sub16x16_dct_lsx x264_template(sub16x16_dct_lsx)
+void x264_sub16x16_dct_lsx( int16_t p_dst[16][16], uint8_t *p_src, uint8_t *p_ref );
+
+#define x264_sub8x8_dct8_lsx x264_template(sub8x8_dct8_lsx)
+void x264_sub8x8_dct8_lsx( int16_t pi_dct[64], uint8_t *p_pix1, uint8_t *p_pix2 );
+#define x264_sub16x16_dct8_lsx x264_template(sub16x16_dct8_lsx)
+void x264_sub16x16_dct8_lsx( int16_t pi_dct[4][64], uint8_t *p_pix1,
+                              uint8_t *p_pix2 );
+
+#define x264_add4x4_idct_lsx x264_template(add4x4_idct_lsx)
+void x264_add4x4_idct_lsx( uint8_t *p_dst, int16_t pi_dct[16] );
+#define x264_add8x8_idct_lsx x264_template(add8x8_idct_lsx)
+void x264_add8x8_idct_lsx( uint8_t *p_dst, int16_t pi_dct[4][16] );
+#define x264_add16x16_idct_lsx x264_template(add16x16_idct_lsx)
+void x264_add16x16_idct_lsx( uint8_t *p_dst, int16_t pi_dct[16][16] );
+#define x264_add8x8_idct8_lsx x264_template(add8x8_idct8_lsx)
+void x264_add8x8_idct8_lsx( uint8_t *p_dst, int16_t pi_dct[64] );
+#define x264_add8x8_idct_dc_lsx x264_template(add8x8_idct_dc_lsx)
+void x264_add8x8_idct_dc_lsx( uint8_t *p_dst, int16_t dct[4] );
+#define x264_add16x16_idct_dc_lsx x264_template(add16x16_idct_dc_lsx)
+void x264_add16x16_idct_dc_lsx( uint8_t *p_dst, int16_t dct[16] );
+
+#define x264_idct4x4dc_lsx x264_template(idct4x4dc_lsx)
+void x264_idct4x4dc_lsx( int16_t d[16] );
+#define x264_dct4x4dc_lsx x264_template(dct4x4dc_lsx)
+void x264_dct4x4dc_lsx( int16_t d[16] );
+
+#define x264_zigzag_scan_4x4_frame_lsx x264_template(zigzag_scan_4x4_frame_lsx)
+void x264_zigzag_scan_4x4_frame_lsx( int16_t level[16], int16_t dct[16] );
+
+#endif
